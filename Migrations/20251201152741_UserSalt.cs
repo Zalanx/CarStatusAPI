9065209e52c3153e20b2bad415e961269f9b18e6
Zalan Bader <Zalan.Bader@scheppach.com>
2025-12-01 16:28:19 +0100

    Added AddScoped services

---
+++ Migrations/20251201152741_UserSalt.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CarStatusAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class UserSalt : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Salt",
+                table: "DbUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Salt",
+                table: "DbUsers");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -13,6 +13,7 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddScoped<ICarStatus, CarStatusRepo>();
+builder.Services.AddScoped<CarStatusRepoHelper>();
 
 builder.Services.AddControllers()
     .AddJsonOptions(options =>
