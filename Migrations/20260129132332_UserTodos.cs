cf81cc59bb9e8cc2a3b200ce49fcccdcc6155415
Zalan Bader <Zalan.Bader@scheppach.com>
2026-01-29 14:25:50 +0100

    new model ToDos in database. latest

---
+++ Migrations/20260129132332_UserTodos.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CarStatusAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class UserTodos : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ToDoDto",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Todo = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    done = table.Column<bool>(type: "bit", nullable: false),
+                    DbUserId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ToDoDto", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ToDoDto_DbUsers_DbUserId",
+                        column: x => x.DbUserId,
+                        principalTable: "DbUsers",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ToDoDto_DbUserId",
+                table: "ToDoDto",
+                column: "DbUserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ToDoDto");
+        }
+    }
+}
